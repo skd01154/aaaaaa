db334fb968263548e140aaf2846ec566f34aed64
skd01154 <128318966+skd01154@users.noreply.github.com>
2023-03-20 05:34:27 +0900

    Update index.php

--- index.php
+++ index.php
@@ -3,9 +3,9 @@
     <meta charset="UTF-8">
     <title>Number Board</title>
     <link rel="stylesheet" href="style.css">
-  </head>
+   </head>
   <body>
     <table id="board"></table>
     <script src="script.js"></script>
-  </body>
+   </body>
 </html>
