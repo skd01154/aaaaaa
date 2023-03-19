5cd4a87cf3761c03f9d12d39e8b5971c01ff14b7
skd01154 <128318966+skd01154@users.noreply.github.com>
2023-03-20 05:33:15 +0900

    Update index.php

--- index.php
+++ index.php
@@ -1,15 +1,10 @@
-<!DOCTYPE html>
 <html>
   <head>
-    <title>PHP</title>
     <meta charset="UTF-8">
     <title>Number Board</title>
     <link rel="stylesheet" href="style.css">
   </head>
   <body>
-<?php
-    echo phpinfo();   
-?>
     <table id="board"></table>
     <script src="script.js"></script>
   </body>
